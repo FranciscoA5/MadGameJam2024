59761e27e94efb7c859caecfd85336f60fd1b2fd
SimplicioLima <49134891+SimplicioLima@users.noreply.github.com>
2024-02-24 14:29:31 +0000

    Screen with colliders

--- MadGameJam2024/Assets/Scripts/Simple/AreaController.cs
+++ MadGameJam2024/Assets/Scripts/Simple/AreaController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AreaController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    [SerializeField] private Transform[] points;
+    [SerializeField] private Transform[] points; //<<<------ Objects with colliders
     [SerializeField] private LineController line;
-    [SerializeField] private Transform firstPos;
+    bool hasEnter1, hasEnter2, hasEnter3, hasEnter4;
     [Space]
     [Header("Box Limits")]
     [SerializeField] private float minDistX;
     [SerializeField] private float minDistY;
+    [SerializeField] private float growDistY;
+    [SerializeField] private float growDistX;
     [Space]
     [Header("Time from Shrinking")]
     [SerializeField] private float timeSpan;
@@ -25,26 +28,26 @@ public class AreaController : MonoBehaviour
     [SerializeField] bool allActive;
     [Space]
     [Header("Shrink Method:\nFalse: Distance Base Speed\nTrue: Identical Shrink Speed")]
-    [SerializeField] bool Method2;
+    [SerializeField] bool ShrinkMode1;
 
     private float velY, velX;
-
+    [SerializeField] private bool ativeCollider;
 
     void Start()
     {
         line.SetUpLine(points);
-        firstPos = transform;
     }
 
     private void Update()
     {
-        if(Method2) ShrinkAreaMethod2();
-        else ShrinkArea();
+        if(ShrinkMode1) ShrinkArea();
+        else ShrinkAreaMethod2();
 
-        if(!Wall1 || !Wall2 || !Wall3 || !Wall4)
-            UpdateLineNewPos();
+        UpdateLineNewPos();
 
         UpdateCenter(speed);
+
+        UpdateColliders();
     }
 
     public void ShrinkAreaMethod2()
@@ -132,30 +135,49 @@ public class AreaController : MonoBehaviour
 
     public void UpdateLineNewPos()
     {
-        
-        if (Wall1)
+        //Left
+        if (Wall1 && !hasEnter1)
         {
-            points[0].position = points[0].position - points[0].right * 0.3f;
-            points[1].position = points[1].position - points[1].right * 0.3f;
-            Wall1 = true;
+            hasEnter1 = true;
+            Vector3 pos1 = new Vector3(points[0].position.x - growDistX , points[0].position.y, 0);
+            Vector3 pos2 = new Vector3(points[1].position.x - growDistX, points[1].position.y, 0);
+            points[0].position = pos1;
+            points[1].position = pos2;
+            ResetValues();
+            Wall1 = false;
         }
-        if (Wall2)
+        //Down
+        if (Wall2 && !hasEnter2)
         {
-            points[1].position = points[1].position - points[1].up * 0.3f;
-            points[2].position = points[2].position - points[2].up * 0.3f;
-            Wall2 = true;
+            hasEnter2 = true;
+            Vector3 pos1 = new Vector3(points[1].position.x, points[1].position.y - growDistY, 0);
+            Vector3 pos2 = new Vector3(points[2].position.x, points[2].position.y - growDistY, 0);
+            points[1].position = pos1;
+            points[2].position = pos2;
+            ResetValues();
+            Wall2 = false;
         }
-        if (Wall3)
+        //Right
+        if (Wall3 && !hasEnter3)
         {
-            points[2].position = points[2].position + points[2].right * 0.3f;
-            points[3].position = points[3].position + points[3].right * 0.3f;
-            Wall3 = true;
+            hasEnter3 = true;
+            Vector3 pos1 = new Vector3(points[2].position.x + growDistX, points[2].position.y, 0);
+            Vector3 pos2 = new Vector3(points[3].position.x + growDistX, points[3].position.y, 0);
+            points[2].position = pos1;
+            points[3].position = pos2;
+            ResetValues();
+            Wall3 = false;
         }
-        if (Wall4)
+        //Up
+        if (Wall4 && !hasEnter4)
         {
-            points[3].position = points[3].position + points[3].up * 0.3f;
-            points[4].position = points[4].position + points[4].up * 0.3f;
-            Wall4 = true;
+            hasEnter4 = true;
+            Vector3 pos1 = new Vector3(points[3].position.x, points[3].position.y + growDistY, 0);
+            Vector3 pos2 = new Vector3(points[0].position.x, points[0].position.y + growDistY, 0);
+            points[3].position = pos1;
+            points[0].position = pos2;
+            ResetValues();
+            Wall4 = false;
         }
 
     }
@@ -165,11 +187,43 @@ public class AreaController : MonoBehaviour
         float posY = (points[0].position.y + points[1].position.y + points[2].position.y + points[4].position.y) / 4;
         float posX = (points[0].position.x + points[1].position.x + points[2].position.x + points[4].position.x) / 4;
 
-
-
         Vector3 newPos = new Vector3(posX, posY, Camera.main.transform.position.z);
         cam.transform.position = newPos;
 
     }
 
+    private async void ResetValues()
+    {
+        await Task.Delay(1000);
+        hasEnter1 = false;
+        hasEnter2 = false;
+        hasEnter3 = false;
+        hasEnter4 = false;
+    }
+
+    
+
+    private void UpdateColliders()
+    {
+        float offset = 1;
+
+        float centerY = Vector2.Distance(points[0].position, points[1].position) / 2;
+        float centerX = Vector2.Distance(points[0].position, points[3].position) / 2;
+
+        float lenghtY = Vector2.Distance(points[0].position, points[1].position);
+        float lenghtX = Vector2.Distance(points[0].position, points[3].position);
+        //wall1
+
+        points[0].GetComponent<BoxCollider2D>().offset = new Vector2(0, -centerY);
+        points[1].GetComponent<BoxCollider2D>().offset = new Vector2(centerX, 0);
+        points[2].GetComponent<BoxCollider2D>().offset = new Vector2(0, centerY);
+        points[3].GetComponent<BoxCollider2D>().offset = new Vector2(-centerX, 0);
+
+        //Update of collider scales
+        points[0].GetComponent<BoxCollider2D>().size = new Vector2(1, lenghtY - offset);
+        points[1].GetComponent<BoxCollider2D>().size = new Vector2(lenghtX - offset, 1);
+        points[2].GetComponent<BoxCollider2D>().size = new Vector2(1, lenghtY - offset);
+        points[3].GetComponent<BoxCollider2D>().size = new Vector2(lenghtX - offset, 1);
+
+    }
 }
