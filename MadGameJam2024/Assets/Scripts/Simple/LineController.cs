49739d3dfaad2b34dd8e2a5975eccf18843fe979
SimplicioLima <49134891+SimplicioLima@users.noreply.github.com>
2024-02-24 19:01:54 +0000

    update screen

--- MadGameJam2024/Assets/Scripts/Simple/LineController.cs
+++ MadGameJam2024/Assets/Scripts/Simple/LineController.cs
@@ -1,30 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class LineController : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private AreaController controller;
+    [SerializeField] private LineRenderer lineRenderer1, lineRenderer2, lineRenderer3, lineRenderer4;
     [SerializeField] private Transform[] points;
+    [SerializeField] private Material[] tex;
 
-    [SerializeField] private bool isFrozen = false;
-
-    void Start()
-    {
-        lineRenderer = GetComponent<LineRenderer>();
-    }
+    bool hasEnter1, hasEnter2, hasEnter3, hasEnter4;
 
     void Update()
     {
-        for (int i = 0; i < points.Length; i++)
+        lineRenderer1.SetPosition(0, points[0].position);
+        lineRenderer1.SetPosition(1, points[1].position);
+        lineRenderer2.SetPosition(0, points[1].position);
+        lineRenderer2.SetPosition(1, points[2].position);
+        lineRenderer3.SetPosition(0, points[2].position);
+        lineRenderer3.SetPosition(1, points[3].position);
+        lineRenderer4.SetPosition(0, points[3].position);
+        lineRenderer4.SetPosition(1, points[0].position);
+
+        if (controller.texWall1 && !hasEnter1)
         {
-            lineRenderer.SetPosition(i, points[i].position);
+            lineRenderer1.material = tex[0];
         }
+        if (controller.texWall2 && !hasEnter2)
+        {
+            hasEnter2 = true;
+            lineRenderer2.material = tex[0];
+        }
+        if (controller.texWall3 && !hasEnter3)
+        {
+            hasEnter3 = true;
+            lineRenderer3.material = tex[0];
+        }
+        if (controller.texWall4 && !hasEnter4)
+        {
+            hasEnter4 = true;
+            lineRenderer4.material = tex[0];
+        }
+
+        SetValues();
+
+        //lineRenderer.material.SetTexture("SpikeWalls", tex[0]);
+    }
+
+    async void SetValues()
+    {
+        await Task.Delay(controller.delaySpike);
+
+        if (hasEnter1) { lineRenderer1.material = tex[1]; hasEnter1 = false; }
+        if (hasEnter2) { lineRenderer2.material = tex[1]; hasEnter2 = false; }
+        if (hasEnter3) { lineRenderer3.material = tex[1]; hasEnter3 = false; }
+        if (hasEnter4) { lineRenderer4.material = tex[1]; hasEnter4 = false; }
     }
 
     public void SetUpLine(Transform[] points)
     {
-        lineRenderer.positionCount = points.Length;
         this.points = points;
     }
 
