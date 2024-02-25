28b952a7453268041c66ac7d122114f3c25e5d96
Francisco Andrade <f-infsec03@hotmail.com>
2024-02-25 00:22:43 +0000

    Caco de vidro update

--- MadGameJam2024/Assets/Scripts/Enemy/RegSteve.cs
+++ MadGameJam2024/Assets/Scripts/Enemy/RegSteve.cs
@@ -44,7 +44,6 @@ public class RegSteve : Enemy
         float randomAngle = Random.Range(0f, Mathf.PI * 2f);
         Vector3 randomDirection = new Vector3(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle), 0);
         Vector3 pos = transform.position + randomDirection * Random.Range(0f, range);
-        Debug.Log("New target position chosen: " + pos);
         return pos;
     }
 }
